63a5c61e8ced4ded145cb3012602d0999f9b9c81
Donatas Abraitis <donatas@vinted.com>
2016-06-10 16:37:40 +0300

    Init commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = 'letsencrypt-redis',
+    version = '0.0.1',
+    description = 'Add challenge token and cert/key pair to Redis',
+    packages = find_packages(),
+    install_requires = [
+        'certbot',
+        'zope.interface',
+    ],
+    entry_points = {
+        'certbot.plugins': [
+          'auth = letsencrypt_redis.authenticator:Authenticator',
+          'installer = letsencrypt_redis.installer:Installer'
+        ],
+    },
+)
