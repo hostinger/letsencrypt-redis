9229bf4538eba51ab56bddd11da0ba786cf7a98e
Donatas Abraitis <donatas@vinted.com>
2016-06-13 21:03:47 +0300

    Add url sections (setup.py)

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import find_packages
 
 setup(
     name = 'letsencrypt-redis',
-    version = '0.0.6',
+    version = '0.0.1',
     description = 'Add challenge token to Redis',
+    url = 'https://github.com/hostinger/letsencrypt-redis',
+    download_url = 'https://github.com/hostinger/letsencrypt-redis/tarball/0.0.1',
     packages = find_packages(),
     install_requires = [
         'certbot',
