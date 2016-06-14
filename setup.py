f831de7663ceea24f5941bb22ff193bc5cd37e6c
Donatas Abraitis <donatas@vinted.com>
2016-06-14 10:12:40 +0300

    Increase version to 0.0.2

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import find_packages
 
 setup(
     name = 'letsencrypt-redis',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'Add challenge token to Redis',
     url = 'https://github.com/hostinger/letsencrypt-redis',
-    download_url = 'https://github.com/hostinger/letsencrypt-redis/tarball/0.0.1',
+    download_url = 'https://github.com/hostinger/letsencrypt-redis/tarball/0.0.2',
     packages = find_packages(),
     install_requires = [
         'certbot',
